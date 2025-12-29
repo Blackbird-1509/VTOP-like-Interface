f99f3a68b6a760a64f773358e95aece64d9a39e9
Blackbird-1509 <sohammisra1509@gmail.com>
2025-12-29 09:08:00 +0530

    Added dynamic forcing to APIs

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+  
 export async function POST(req: Request) {
   try {
     const body = await req.json();
